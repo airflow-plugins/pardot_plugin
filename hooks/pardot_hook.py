373d20cbeecc1df77920f39016cec31300587aaf
Benjamin Gregory <benjaminrgregory@gmail.com>
2018-01-25 09:57:42 -0600

    add support for multiple api versions

--- hooks/pardot_hook.py
+++ hooks/pardot_hook.py
@@ -1,6 +1,19 @@
+"""
+Pardot Hook
+
+NOTE: This hook requires a modified version of the PyPardot4 package that
+allows the version to be manually set to v3 or v4. This modified version
+can be found here: https://github.com/astronomerio/PyPardot4 and can be added
+to your environment by including the following in your requirements.txt:
+`-e git+https://github.com/astronomerio/PyPardot4.git#egg=pypardot`
+
+There is an open PR in PyPardot4 to make this change permanent:
+https://github.com/mneedham91/PyPardot4/pull/13
+"""
+
 from airflow.hooks.base_hook import BaseHook
 from pypardot.client import PardotAPI
-import json
+import logging
 
 
 class PardotHook(BaseHook):
@@ -20,43 +33,64 @@ class PardotHook(BaseHook):
     def get_conn(self):
         """
         Initialize a pardot instance.
+        :param email:       The email address associated with the account.
+        :type string:       string
+        :param password:    The password associated with the account.
+        :type string:       string
+        :param user_key:    The issued user_key associated with the account.
+        :type user_key:     string
+        :param api_version: The version of the Pardot API that the account uses.
+                            This is set on the account level within Pardot.
+                            Possible values include:
+                                - 3
+                                - 4
+        :type api_version:  integer
         """
         if self.pardot:
             return self.pardot
 
         self.connection = self.get_connection(self.conn_id)
         self.extras = self.connection.extra_dejson
-        pardot = PardotAPI(email=self.extras['username'],
+        pardot = PardotAPI(email=self.extras['email'],
                            password=self.extras['password'],
-                           user_key=self.extras['user_key'])
+                           user_key=self.extras['user_key'],
+                           version=self.extras['api_version'])
 
         pardot.authenticate()
         self.pardot = pardot
 
         return pardot
 
-    def run_query(self, model, results_field, replication_key_value, method_to_call='query', **kwargs):
+    def run_query(self,
+                  pardot_obj,
+                  results_field,
+                  method_to_call='query',
+                  **kwargs):
         """
         Run a query against pardot
-        :param model:                   name of the Pardot model
-        :param results_field:           name of the results array from 
+        :param object:                   name of the Pardot object
+        :param results_field:           name of the results array from
                                         response
-        :param replication_key_value:   pardot replicaton key
         """
         pardot = self.get_conn()
-        pardot_model = getattr(pardot, model)
+        pardot_object = getattr(pardot, pardot_obj)
 
-        result = getattr(pardot_model, method_to_call)(
-            id_greater_than=replication_key_value, **kwargs)
+        result = getattr(pardot_object, method_to_call)(**kwargs)
 
         offset = 0
         total_results = result[results_field]
 
         while len(total_results) < result['total_results']:
             offset += len(result[results_field])
-            result = getattr(pardot_model, method_to_call)(
-                id_greater_than=replication_key_value, offset=offset, **kwargs)
+            result = getattr(pardot_object, method_to_call)(offset=offset,
+                                                            **kwargs)
             total_results += result[results_field]
+            logging.info('Retrieved: ' + str(offset) + ' records.')
+
+        if total_results is not None:
+            logging.info('Retrieved: ' +
+                         str(len(total_results)) +
+                         ' total records.')
 
         return total_results
 
